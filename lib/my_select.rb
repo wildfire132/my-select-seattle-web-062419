78d78231079a35b1830d53bc334f7001f86fa3cb
Katana Tran <tkatyk21@gmail.com>
2019-06-21 17:12:55 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,6 +2,8 @@ def my_select(collection)
  i = 0
  selection = []
   while i < collection.length
-    if 
-      yield(array[i])
+    if yield(array[i])
+      selection << yield(array[i])
+    end
+  end 
 end
